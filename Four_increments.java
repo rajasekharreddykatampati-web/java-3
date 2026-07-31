d4d2ad59e825211dfa8b84da6c7de29b38b061d6
rajasekharreddykatampati-web <rajasekharreddykatampati@gmail.com>
2026-07-31 18:02:09 +0530

    Add files via upload

---
+++ Four_increments.java
@@ -0,0 +1,19 @@
+class Four_increments.java {
+    public static void main(String args[]) {
+        int a = 10, b = 5, c = 3, d = 3;
+        if((a>b) && (c == ++d))
+        {
+            System.out.println(a++);
+            System.out.println(--b);
+            System.out.println(++c);
+            System.out.println(d--);
+        }
+        else
+        {
+            System.out.println(a++);
+            System.out.println(b);
+            System.out.println(++c);
+            System.out.println(d);
+        }
+    }
+}
